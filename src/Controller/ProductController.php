b0f3197e64114873a934e44c99264350f259d750
Sebastien Colbert <colbertsebastien@gmail.com>
2019-11-06 05:53:51 +0100

    Ajout des différentes routes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,11 +8,51 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/", name="product")
+     * @Route("/", name="home")
      */
     public function index()
     {
+        return $this->render('product/home.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    /**
+     * @Route("/shop", name="shop")
+     */
+    public function shop()
+    {
+        return $this->render('product/shop.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    /**
+     * @Route("/product-{id}", name="product")
+     */
+    public function product($id)
+    {
         return $this->render('product/product.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    /**
+     * @Route("/cart", name="cart")
+     */
+    public function cart()
+    {
+        return $this->render('product/cart.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    /**
+     * @Route("/checkout", name="checkout")
+     */
+    public function checkout()
+    {
+        return $this->render('product/checkout.html.twig', [
             'controller_name' => 'ProductController',
         ]);
     }
