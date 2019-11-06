de0463daf92349700227936b372f484341d5b5ff
Sebastien Colbert <colbertsebastien@gmail.com>
2019-11-06 07:05:35 +0100

    Ajout du CRUD de base

--- src/Controller/AdminProductController.php
+++ src/Controller/AdminProductController.php
@@ -2,18 +2,124 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminProductController extends AbstractController
 {
     /**
-     * @Route("/admin/product", name="admin_product")
+     * @Route("/admin/", name="admin")
      */
     public function index()
     {
-        return $this->render('admin_product/index.html.twig', [
+        return $this->render('admin/product/dashboard.html.twig', [
             'controller_name' => 'AdminProductController',
         ]);
+    }
+
+
+    /**
+     * @Route("/admin/product", name="adminProduct")
+     */
+    public function list(ProductRepository $productRepository)
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('admin/product/list.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/product/create", name="adminProductCreate")
+     */
+    public function create(Request $request)
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $product = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('adminProductList');
+        }
+
+        return $this->render('admin/product/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/product/read-{id}", name="adminProductRead")
+     */
+    public function read($id, ProductRepository $productRepository)
+    {
+        $product = $productRepository->find($id);
+
+        return $this->render('admin/product/edit.html.twig', [
+            'product' => $product
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/product/edit-{id}", name="adminProductEdit")
+     */
+    public function edit(Request $request, ProductRepository $productRepository, $id)
+    {
+        $product = $productRepository->find($id);
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $product = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('adminProductList');
+        }
+
+        return $this->render('admin/product/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+    /**
+     * @Route("//admin/product/del-{id}", name="adminProductDel")
+     */
+    public function delete(ObjectManager $manager, ProductRepository $productRepository, $id)
+    {
+        $product = $productRepository->find($id);
+
+        $manager->remove($product);
+        $manager->flush();
+
+        return $this->redirectToRoute('adminProductList');
     }
 }
